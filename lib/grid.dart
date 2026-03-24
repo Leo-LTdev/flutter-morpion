21db69aca4da8769a7dfaa79998a072be6bcd725
Matth-kaiton <matthias.dumas@ynov.com>
2026-03-24 17:34:27 +0100

    morpion

--- lib/grid.dart
+++ lib/grid.dart
@@ -26,6 +26,8 @@ class MyGrid extends StatefulWidget{
 
 class _MyGridSate extends State<MyGrid> {
 
+  final size = 3;
+
   List<List<StateCase>> board = [
     for (int row = 0; row < 3; row++ ) [
       for(int col = 0; col < 3; col++)
@@ -34,14 +36,92 @@ class _MyGridSate extends State<MyGrid> {
     ];
 
   bool firstPlayer = true;
-  
-  
 
+  void isWinner(){
 
-  void placePawn(int row, int col){
+    StateCase player;
+    if (firstPlayer){
+      player = StateCase.cross;
+    } else {
+      player = StateCase.circle;
+    }
     
+    var rowWin = false;
+
+    for(int row = 0; row < size; row++ ){
+      rowWin = checkRow(row, 0, player, 0);
+      if (rowWin){
+        break;
+      }
+    }
+
+    var colWin = false;
+
+    for(int col = 0; col < size; col++ ){
+      colWin = checkCol(0, col, player, 0);
+      print(colWin);
+      if (colWin){
+        break;
+      }
+    }
+
+   
+    
+    var diagWing;
+    var reverseDiagWin;
+
+    if(rowWin || colWin){
+      getWinningPlayer();
+    }
+
+
   }
 
+  bool checkRow(int row, int col, StateCase player, int counter){
+
+    if (col > size ){
+      return false;
+    }
+
+    if(counter == size){
+      return true;
+    }
+
+    if (player == board[row][col]){
+      counter++;
+      col++;
+      return checkRow(row, col, player, counter);
+    }
+
+    return false;
+  }
+  
+  bool checkCol(int row, int col, StateCase player, int counter){
+    
+    if (row > size ){
+      return false;
+    }
+
+    if(counter == size){
+      return true;
+    }
+
+    if (player.placeOlder == board[row][col].placeOlder){
+      counter++;
+      row++;
+      return checkRow(row, col, player, counter);
+    }
+
+    return false;
+  }
+
+  void getWinningPlayer(){
+    if (firstPlayer){
+      print("Joueur 1 à gagné"); 
+    } else {
+      print("Joueur 2 à gagné");
+    }
+  }
 
   @override
   Widget build(BuildContext context){
@@ -62,9 +142,11 @@ class _MyGridSate extends State<MyGrid> {
                       setState(() {
                         if (firstPlayer){
                           board[i][j] = StateCase.cross;
+                          isWinner();
                           firstPlayer = !firstPlayer;
                         } else {
                           board[i][j] = StateCase.circle;
+                          isWinner();
                           firstPlayer = !firstPlayer;
                         }
                       });
