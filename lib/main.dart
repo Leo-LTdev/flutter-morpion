e96b3b928fb258e4d31a21c471478789c699a078
Stiximir <stiximir100@gmail.com>
2026-03-26 16:12:33 +0100

    hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,28 @@
-  import 'package:flutter/material.dart';
-  import 'package:flutter_application_1/apiDB.dart';
-  import 'package:flutter_application_1/grid.dart';
-  import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/apiDB.dart';
+import 'package:flutter_application_1/grid.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-  Future<void> main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Supabase.initialize(
-      url: 'https://dmvvzunzfafdsbcgnubf.supabase.co',
-      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtdnZ6dW56ZmFmZHNiY2dudWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNTg1MjQsImV4cCI6MjA4OTkzNDUyNH0.7I5i5hEAII1zqVpQYKUyieuW-HNw6nTBUCip7uwqVVM',
-    );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://dmvvzunzfafdsbcgnubf.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtdnZ6dW56ZmFmZHNiY2dudWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNTg1MjQsImV4cCI6MjA4OTkzNDUyNH0.7I5i5hEAII1zqVpQYKUyieuW-HNw6nTBUCip7uwqVVM',
+  );
 
-    runApp(const MyApp());
-  }
+  runApp(
+    MaterialApp(
+      home: MyGrid(),
+    ),
+  );
+}
 
-  var mult = 1;
+var mult = 1;
 
-  @override
-  Widget build(BuildContext context) {
-    const title = 'Grid List';
-    
+@override
+Widget build(BuildContext context) {
+  const title = 'Grid List';
 
-    return MaterialApp(
-      title: title,
-      home: MyGrid());
-  }
+  return MaterialApp(title: title, home: MyGrid());
+}
